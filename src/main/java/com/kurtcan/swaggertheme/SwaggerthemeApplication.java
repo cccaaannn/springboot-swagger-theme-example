e9836c7cdf4a650d8e3c6ece50931c0fd96ddbe7
cccaaannn <can.kurt.aa@gmail.com>
2026-05-03 02:21:13 +0300

    first commit

---
+++ src/main/java/com/kurtcan/swaggertheme/SwaggerthemeApplication.java
@@ -0,0 +1,13 @@
+package com.kurtcan.swaggertheme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerthemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerthemeApplication.class, args);
+	}
+
+}
